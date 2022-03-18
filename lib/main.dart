07a7a00d67fd4cb1d397e2d6ad693eb0c6acc79e
Angyee Alexandra <78864768+Angxandralol@users.noreply.github.com>
2022-03-18 17:59:58 -0400

    Creacion de Segundo Reto del curso de flutter de Platzi

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,39 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: Row(
+            children: <Widget>[
+              const FlutterLogo(),
+              const Text(" Share"),
+            ],
+          ),
+        ),
+        body: Stack(
+          children: <Widget>[
+            Container(
+              width: 500,
+              height: 800,
+              color: Colors.black54,
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+      //const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
